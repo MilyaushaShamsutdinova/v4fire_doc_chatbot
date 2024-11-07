a0be1d64e2cee715c496efd45b53852ae5907471
MilyaushaShamsutdinova <m.shamsutdinova@innopolis.university>
2024-11-08 00:25:53 +0300

    added serving in tg bot

---
+++ src/frontend/bot.py
@@ -0,0 +1,40 @@
+import os
+import telebot
+from dotenv import load_dotenv
+import re
+from src.backend.pipeline import get_response
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def format_text(text):
+    """
+    Converts markdown-style text into a more natural format for Telegram,
+    removing markdown symbols and ensuring appropriate spacing.
+    """
+    text = re.sub(r'\*\*(.*?)\*\*', r'\1', text)
+    text = re.sub(r'\*(.*?)\*', r'\1', text)
+    text = re.sub(r'\*\s', '- ', text)
+    text = re.sub(r'(\d+)\.\s', r'\1) ', text)
+    text = re.sub(r'##', '', text)
+    text = re.sub(r'(\n\s*)\n', r'\1', text)
+    text = re.sub(r'(^|\n)([^\n-])', r'\1\n\2', text)
+    return text.strip()
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, f"Hi, {message.from_user.first_name}!\nI am V4Fire assistant, i can help you to write components\nAsk any question related to V4Fire :)")
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    request = message.text
+    response = get_response(request)
+    bot.send_message(message.chat.id,
+                     response,
+                     parse_mode='Markdown')
+
+def run_bot():
+    bot.infinity_polling()
